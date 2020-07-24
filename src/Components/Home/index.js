090386a16bc9bcaccf8ee90e14d1af9c4a2a5ca8
Ian Kaliszewski <ikalisz@yahoo.com>
2020-07-24 11:28:11 -0600

    implemented upgrade

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -15,19 +15,37 @@ function Home() {
   const [
     clickPower,
     setClickPower,
-  ] = useState(5)
+  ] = useState(1)
+  const [
+    cpUpgrade,
+    setCPUpgrade,
+  ] = useState(30)
 
   if(isLoading) {
     return
   }
 
   const handleClick = (modifier) => {
-    console.log('here')
-    const increaseAmount = 1 * modifier
-    const newWaifus = waifus + increaseAmount
+    let increaseAmount = 1 * modifier
+    let newWaifus = waifus + increaseAmount
     setWaifus(newWaifus)
     return
   }
+
+  const handleNoAffordUpgrade = () => {
+    return window.alert("You can't afford this upgrade!")
+  }
+
+  const handleCPUpgrade = () => {
+    let newWaifus = waifus - cpUpgrade
+    let newCPUpgrade = cpUpgrade * (1/clickPower * clickPower + 1)
+    let newCP = clickPower + 1
+    setWaifus(newWaifus)
+    setCPUpgrade(newCPUpgrade)
+    setClickPower(newCP)
+  }
+
+  const cpUpgradeAvailable = waifus >= cpUpgrade
 
   const clickerComponent = waifus <= 100 ?
     'https://cdn.discordapp.com/attachments/565367162542358538/612932010654171156/Re1.png': waifus <= 200 ?
@@ -36,32 +54,44 @@ function Home() {
 
   return (
     <SContainer>
-      <SClicker
-        src={clickerComponent}
-        alt="iiLikey's OC"
-        onClick={() => handleClick(clickPower)}
-      />
-      <SWaifus>
-        {waifus}
-      </SWaifus>
-      <div>
-        <button
-          style={{
-            display: 'none',
-          }}
-          onClick={setLoading}
-        >
-          Ignore me
-        </button>
-        <button
-          style={{
-            display: 'none',
-          }}
-          onClick={setClickPower}
-        >
-          Ignore me
-        </button>
-      </div>
+      <SClickerContainer>
+        <SClicker
+          src={clickerComponent}
+          alt="iiLikey's OC"
+          onClick={() => handleClick(clickPower)}
+        />
+        <SWaifus>
+          {waifus}
+        </SWaifus>
+        <div>
+          <button
+            style={{
+              display: 'none',
+            }}
+            onClick={setLoading}
+          >
+            Ignore me
+          </button>
+          <button
+            style={{
+              display: 'none',
+            }}
+            onClick={setClickPower}
+          >
+            Ignore me
+          </button>
+        </div>
+      </SClickerContainer>
+      <SUpgradeContainer>
+          <SCPUpgrade
+            cpUpgradeAvailable={cpUpgradeAvailable}
+            onClick={cpUpgradeAvailable ? handleCPUpgrade : handleNoAffordUpgrade}
+          >
+            <SUpgrade>
+              Click Power Upgrade: {cpUpgrade} waifus
+            </SUpgrade>
+          </SCPUpgrade>
+      </SUpgradeContainer>
     </SContainer>
   )
 }
@@ -70,15 +100,43 @@ export default Home
 
 const SContainer = styled.div`
   display: flex;
+  flex-direction: row;
+  justify-content: space-inbetween;
+  width: 100vw;
+`
+
+const SClicker = styled.img`
+  margin-bottom: 30px;
+`
+
+const SWaifus = styled.p`
+  color: black;
+`
+
+const SClickerContainer = styled.div`
+  display: flex;
   flex-direction: column;
   justify-content: space-evenly;
   align-items: center;
   width: 100vw;
 `
 
-const SClicker = styled.img`
+const SUpgradeContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: right;
 `
 
-const SWaifus = styled.p`
-  color: black;
+const SCPUpgrade = styled.div`
+  height: 100px;
+  width: 200px;
+  border: 1px black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  opacity: ${(props) => props.cpUpgradeAvailable ? 1 : .3};
+`
+
+const SUpgrade = styled.p`
+
 `
